f9f7a2b419a78e9c93cdad4ce973b4fc1c59fdf9
lzb <271526143@qq.com>
2017-05-20 23:09:13 +0800

    tiny4412:led service app add

---
+++ ledService/app_1th_led/app/src/main/java/com/example/hasee/app_1th_led/MainActivity.java
@@ -0,0 +1,193 @@
+package com.example.hasee.app_1th_led;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+import android.os.ILedService;
+import android.os.ServiceManager;
+
+public class MainActivity extends Activity {
+
+    private Button allButton = null;
+    private CheckBox checkBox1 = null;
+    private CheckBox checkBox2 = null;
+    private CheckBox checkBox3 = null;
+    private CheckBox checkBox4 = null;
+    private boolean allChecked = false;
+    private ILedService ledService = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ledService = ILedService.Stub.asInterface(ServiceManager.getService("led"));
+
+        final Button allButton = (Button) findViewById(R.id.AllButton);
+        final CheckBox checkBox1 = (CheckBox)findViewById(R.id.CheckBox_led1);
+        final CheckBox checkBox2 = (CheckBox)findViewById(R.id.CheckBox_led2);
+        final CheckBox checkBox3 = (CheckBox)findViewById(R.id.CheckBox_led3);
+        final CheckBox checkBox4 = (CheckBox)findViewById(R.id.CheckBox_led4);
+
+        allButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                if (allChecked == true)
+                {
+                    allChecked = false;
+                    allButton.setText("All Button off");
+                    Toast.makeText(getApplicationContext(), "All Button off",
+                            Toast.LENGTH_SHORT).show();
+                    checkBox1.setChecked(false);
+                    checkBox2.setChecked(false);
+                    checkBox3.setChecked(false);
+                    checkBox4.setChecked(false);
+                    try {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            ledService.ledCtrl(i, 0);
+                        }
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else
+                {
+                    allChecked = true;
+                    Toast.makeText(getApplicationContext(), "All Button on",
+                            Toast.LENGTH_SHORT).show();
+                    allButton.setText("ALL Button on");
+                    checkBox1.setChecked(true);
+                    checkBox2.setChecked(true);
+                    checkBox3.setChecked(true);
+                    checkBox4.setChecked(true);
+                    try {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            ledService.ledCtrl(i, 1);
+                        }
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Check which checkbox was clicked
+        boolean checked = ((CheckBox) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.CheckBox_led1:
+                if (!checked) {
+                    Toast.makeText(getApplicationContext(), "led1 off",
+                            Toast.LENGTH_SHORT).show();
+                    try {
+                        ledService.ledCtrl(0, 0);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "led1 on",
+                            Toast.LENGTH_SHORT).show();
+                    try {
+                        ledService.ledCtrl(0, 1);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case R.id.CheckBox_led2:
+                if (!checked) {
+                    Toast.makeText(getApplicationContext(), "led2 off",
+                            Toast.LENGTH_SHORT).show();
+                    try {
+                        ledService.ledCtrl(1, 0);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "led2 on",
+                            Toast.LENGTH_SHORT).show();
+                    try {
+                        ledService.ledCtrl(1, 1);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case R.id.CheckBox_led3:
+                if (!checked) {
+                    Toast.makeText(getApplicationContext(), "led3 off",
+                            Toast.LENGTH_SHORT).show();
+                    try {
+                        ledService.ledCtrl(2, 0);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "led3 on",
+                            Toast.LENGTH_SHORT).show();
+                    try {
+                        ledService.ledCtrl(2, 1);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case R.id.CheckBox_led4:
+                if (!checked) {
+                    Toast.makeText(getApplicationContext(), "led4 off",
+                            Toast.LENGTH_SHORT).show();
+                    try {
+                        ledService.ledCtrl(3, 0);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "led4 on",
+                            Toast.LENGTH_SHORT).show();
+                    try {
+                        ledService.ledCtrl(3, 1);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
